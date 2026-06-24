bf379419ffff75e93c7951897d92b3d7acba07b2
Thyen2309 <thienx2duy006@gmail.com>
2026-06-25 06:25:42 +0700

    day1

---
+++ day1.py
@@ -0,0 +1,13 @@
+name = "Nguyễn Duy Thiện"
+age = 20
+height = 1.72
+is_programmer = True
+print(f'Tên tôi là: {name}')
+print(f'Tuổi của tôi là: {age}')
+print(f'Chiều cao của tôi là: {height}')
+print(f'Tôi có phải là lập trình viên không? {is_programmer}')
+#BÀI2
+print('Loại của name là: ',type(name))
+print('loại của tuổi là: ',type(age))
+print('loại của chiều cao là: ',type(height))
+print('loại đúng hay sai của programmer',type(is_programmer))
